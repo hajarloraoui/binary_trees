f215ecb2e1414aa817643c6fa6a43150339b1832
hajarloraoui <134793896+hajarloraoui@users.noreply.github.com>
2024-02-01 21:56:24 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,13 +2,12 @@
 #include "binary_trees.h"
 
 /**
- * Function: binary_tree_node
- * Description: Creates a node for a binary tree
- * @parent: Pointer to the parent node of the new node
- * @value: Value to be assigned to the new node
- *
- * Returns: Pointer to the newly created node, or NULL if creation fails
- */
+* Function: binary_tree_node
+* @parent: Pointr to the parent node of the new node
+* @value: Value to be assigned to the new node
+*
+* Returns: Pointr to the newly created node, or NULL if creation fails
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
