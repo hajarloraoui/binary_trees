049bc6b47bfb3ef3af7225bcfddfb74a1c94aff3
hajarloraoui <134793896+hajarloraoui@users.noreply.github.com>
2024-02-01 21:27:10 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,10 +2,10 @@
 
 
 /**
- * binary_tree_is_leaf - Checks if a node is a leaf.
- * @node: Pointer to the node to check.
- * 
- * Return: 1 if it is a leaf, 0 otherwise. If the node is NULL, returns 0.
+* binary_tree_is_leaf - Checks if a node is a leaf.
+* @node: Pointer to the node to check.
+* 
+* Return: 1 if it is a leaf, 0 otherwise. If the node is NULL, returns 0.
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
