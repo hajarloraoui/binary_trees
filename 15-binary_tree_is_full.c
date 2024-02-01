eb74fa787d1bfa2db6ec8cef996958b2a2ae36c2
hajarloraoui <134793896+hajarloraoui@users.noreply.github.com>
2024-02-01 21:23:55 +0100

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - Verifica se un albero binario è completo
- *
- * @tree: Puntatore al nodo radice dell'albero
- * Return: 1 se l'albero è completo, altrimenti 0.
- */
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Pointer to the root node of the tree
+ * 
+ * Return: 1 if the tree is full, otherwise 0.
+*/
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
